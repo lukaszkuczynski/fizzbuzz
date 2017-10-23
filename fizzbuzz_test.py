eada8f1913157c1c7eeef54ecf4d0e44819ac2bc
kuczynskilukasz <kuczynskilukasz@gmail.com>
2017-10-23 17:33:42 +0200

    5_red

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -11,5 +11,10 @@ class FizzBuzzTest(TestCase):
         actual = self.sut.run_for(3)
         self.assertEquals(actual, 'fizz')
 
+    def test_for_5_returns_buzz(self):
+        actual = self.sut.run_for(5)
+        self.assertEquals(actual, 'buzz')
+
+
 if __name__ == '__main__':
     main()
--- fizzbuzzer.py
+++ fizzbuzzer.py
@@ -3,4 +3,4 @@ class FizzBuzzer:
     def run_for(self, number):
         if number % 3 == 0:
             return 'fizz'
-
+        # elif number % 5
