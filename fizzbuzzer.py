b319d341a576f9244269a9e853d6631c7222e190
kuczynskilukasz <kuczynskilukasz@gmail.com>
2017-10-23 17:34:07 +0200

    5_green

--- fizzbuzzer.py
+++ fizzbuzzer.py
@@ -3,4 +3,5 @@ class FizzBuzzer:
     def run_for(self, number):
         if number % 3 == 0:
             return 'fizz'
-        # elif number % 5
+        elif number % 5 == 0:
+            return 'buzz'
