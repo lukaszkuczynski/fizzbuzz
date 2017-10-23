a6cd8b73e699dc193e1824d2fa19ad31c1ce37bb
kuczynskilukasz <kuczynskilukasz@gmail.com>
2017-10-23 18:03:04 +0200

    diagonal OK

--- cross_circle.py
+++ cross_circle.py
@@ -6,10 +6,16 @@
 
 class CrossCircle:
     def who_won(self, scores):
-        return self.diagonal(scores) or self.row(scores) or self.column(scores)
+        return \
+            self.diagonal(scores) \
+            or self.row(scores) \
+            or self.column(scores)
 
     def diagonal(self, scores):
-        return None
+        one_diag = scores[0][0] + scores[1][1] + scores[2][2]
+        other_diag = scores[0][2] + scores[1][1] + scores[2][0]
+        if one_diag == 0 or other_diag == 0: return 'circle'
+        elif one_diag == 3 or other_diag == 3: return 'cross'
 
     def row(self, scores):
         for row in scores:
@@ -29,5 +35,6 @@ class CrossCircle:
 
 if __name__ == '__main__':
     cc = CrossCircle()
-    won = cc.who_won([[0,0,1],[0,1,1],[1,0,1]])
+    # won = cc.who_won([[0,0,1],[0,1,1],[1,0,1]])
+    won = cc.who_won([[0, 0, 1], [0, 0, 1], [1, 1, 0]])
     print(won)
